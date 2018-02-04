b8349a34ee1950b021576f51d6ea1d13ffe1bba4
Elisha Sarkis <elisha.sarkis96@gmail.com>
2018-02-04 15:34:47 -0500

    Quality of life helper functions

--- Observer/Subject.cpp
+++ Observer/Subject.cpp
@@ -1,5 +1,11 @@
 #include "Subject.h"
 
+Subject::~Subject()
+{
+	m_observers.clear();
+	m_observers.shrink_to_fit();
+}
+
 void Subject::addObserver(Observer* t_observer)
 {
 	m_observers.push_back(t_observer);
@@ -8,14 +14,21 @@ void Subject::addObserver(Observer* t_observer)
 void Subject::removeObserver(Observer* t_observer)
 {
 	if (m_observers.empty()) return;
-	for (int i = 0; i < getNumObservers(); i++)
+	for (int i = 0; i < Subject::getNumObservers(); i++)
 	{
 		if (m_observers[i] == t_observer)
 		{
 			m_observers.erase(m_observers.begin() + i);
+			m_observers.shrink_to_fit();
 			return;
 		}
 	}
+}
+
+void Subject::removeAllObservers()
+{
+	m_observers.clear();
+	m_observers.shrink_to_fit();
 }
 
 int Subject::getNumObservers() const
@@ -25,7 +38,7 @@ int Subject::getNumObservers() const
 
 void Subject::notify(const Entity &t_entity, Event t_event)
 {
-	// Assumption: Observers do not modify list in onNotify methods
+	// Assumption: Observers do not modify m_observers in onNotify methods
 	for (int i = 0; i < Subject::getNumObservers(); i++)
 	{
 		m_observers[i]->onNotify(t_entity, t_event);
--- Observer/Subject.h
+++ Observer/Subject.h
@@ -3,18 +3,23 @@
 
 #include "Observer.h"
 
-
+// Provides notifications to subscribed Observers
 class Subject
 {
 public:
+	~Subject();
+	// Add a given pointer to an Observer to Observer collection
 	void addObserver(Observer*);
+	// Remove a given pointer to an Observer from Observer collection
 	void removeObserver(Observer*);
-
+	// Remove all pointers to Observers from Observer collection
+	void removeAllObservers();
+	// Return the total number of Observers observing this Subject
 	int getNumObservers() const;
-
 protected:
+	// Notify all Observers in Observers Collection of given Event
 	void notify(const Entity&, Event);
 private:
-	std::vector<Observer*> m_observers;
+	std::vector<Observer*> m_observers; // Collection of Observers to recieve notifications
 };
 
